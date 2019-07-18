259f8667b281c4eb0740b3d4ff3456fa170257c6
Milan Shah <milanshah1@gmail.com>
2019-07-18 17:30:31 +0530

    Updates

---
+++ precache-manifest.688aa94daf44a878f57b7aab2ac7b2a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec362fc2dee3b601bed393061ddf01ba",
+    "url": "/useranalyticsdashboard/index.html"
+  },
+  {
+    "revision": "962f255181b10c75383f",
+    "url": "/useranalyticsdashboard/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "ac6b8cf98280f2404773",
+    "url": "/useranalyticsdashboard/static/css/main.aadd5ce6.chunk.css"
+  },
+  {
+    "revision": "962f255181b10c75383f",
+    "url": "/useranalyticsdashboard/static/js/2.26d0fd14.chunk.js"
+  },
+  {
+    "revision": "ac6b8cf98280f2404773",
+    "url": "/useranalyticsdashboard/static/js/main.ca705d66.chunk.js"
+  },
+  {
+    "revision": "a2fbd37f458f06457151",
+    "url": "/useranalyticsdashboard/static/js/runtime~main.23feea0e.js"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/useranalyticsdashboard/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/useranalyticsdashboard/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/useranalyticsdashboard/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/useranalyticsdashboard/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
